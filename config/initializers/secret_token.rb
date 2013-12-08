fafc376fb13f9fa9f422e06ef49e169050bcba67
Tyler DeWitt <tyler@Tylers-MacBook-Pro.local>
2013-12-07 20:44:25 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DaFrack::Application.config.secret_key_base = '116dedb7a89d8f398d039d19f24563e1a3eea523aaa2c140514021cb2aef5551dbc872b5cbf99daed680c8f31e4b5d0a266054bf6b56ac68d68bfb4c4d298510'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DaFrack::Application.config.session_store :cookie_store, key: '_da_frack_session'

55d37fc94ba308b81794aa38d60dd7d659526766
Tyler DeWitt <tyler.dewitt@colorado.edu>
2013-12-13 10:55:17 -0600

    authentication loops make much more sense

--- app/controllers/wells_controller.rb
+++ app/controllers/wells_controller.rb
@@ -1,4 +1,5 @@
 class WellsController < ApplicationController
+  before_filter :authenticate_user!
   before_action :set_well, only: [:show, :edit, :update, :destroy]
   load_and_authorize_resource
 

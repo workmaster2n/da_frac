3981ff3f7c202c2caf04b5574a607aad2d12b36d
Tyler DeWitt <tyler.dewitt@colorado.edu>
2013-12-13 10:34:42 -0600

    redirectingt to sign in if not authorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_path, :alert => exception.message
+  before_filter :store_location
+
+  def store_location
+    # store last url as long as it isn't a /users path
+    session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
   end
 
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to new_user_session_url, :alert => exception.message
+  end
 end
